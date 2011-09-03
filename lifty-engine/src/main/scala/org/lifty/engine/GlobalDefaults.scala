9e63bf3ddb08efedb0a1d7c2c518ee8dcd4a4e69
Mads Hartmann <mads379@gmail.com>
2011-09-03 23:57:23 +0200

    Updating the tempaltes to use SBT 10. Committing so I can test properly

--- lifty-engine/src/main/scala/org/lifty/engine/GlobalDefaults.scala
+++ lifty-engine/src/main/scala/org/lifty/engine/GlobalDefaults.scala
@@ -18,21 +18,33 @@ object GlobalDefaults {
   
   val Pattern = """\$\{(.*)\}(.*)""".r
   
-  def replace(str: String) = str match {
-    case Pattern("PACKAGE",rest) => searchForMainPackage().map(_+rest).getOrElse({ // screw IO ;) 
-      println("Couldn't find a build.properties file so can't supply a default value for the main package")
+  def replace(str: String) = {
+  
+    def f(opt: Option[String], other: String) = opt.map(_+other).getOrElse({
+      println("Couldn't find a build.properties file so can't supply a default value for the argument")
       ""
     })
-    case _ => str
+    
+    str match {
+      case Pattern("PACKAGE",rest)       => f(searchForMainPackage(), rest)
+      case Pattern("SCALA_VERSION",rest) => f(searchForScalaVersion(), rest)
+      case Pattern("PROJECT_NAME",rest)  => f(searchForProjectName(), rest)
+      case _                             => str
+    }
   }
   
-  private def searchForMainPackage(): Option[String] = {
+  private def searchForMainPackage(): Option[String] = search("project.organization")
+  
+  private def searchForScalaVersion(): Option[String] = search("build.scala.versions")
+  
+  private def searchForProjectName(): Option[String] = search("project.name")
+  
+  private def search(property: String) = {
     val properties = new Properties()
     properties.load(new FileInputStream("project"+File.separator+"build.properties"))
-    properties.getProperty("project.organization") match {
+    properties.getProperty(property) match {
       case null => None
       case str => Some(str)
     }
   }
-  
 }
