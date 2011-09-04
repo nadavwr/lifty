8eda24b577725fa5e12ddeda235df3f8f0adcba6
Mads Hartmann <mads379@gmail.com>
2011-09-04 21:31:54 +0200

    Updated the code to use lifty-engine properly

--- lifty-plugin/Lifty.scala
+++ lifty-plugin/Lifty.scala
@@ -4,6 +4,7 @@ import java.net.{ URL }
 import org.lifty.engine._
 import sbt._
 import Keys._
+import xsbti.{Logger}
 
 object Lifty extends Plugin {
 
@@ -11,13 +12,16 @@ object Lifty extends Plugin {
 
   lazy val liftyCommand =
     Command.args("lifty","<help>") { (state, args) =>
-      val url = new File("lifty-engine/src/test/resources/test-descriptor.json").toURI.toURL
-      val liftyEngineInstance = new LiftyEngineInstance(url)
-      liftyEngineInstance.run(args).fold(
-        e => println(e.message),
-        s => println(s)
+      LiftyInstance.run(args.toList).fold(
+        e => {
+          println(e.message)
+          state.fail
+        },
+        s => {
+          println(s)
+          state
+        }
       )
-      state
     }
 
 }
