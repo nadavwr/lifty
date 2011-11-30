52e13125579c9d0bbc89fe20615f3107d081a602
Mads Hartmann <mads379@gmail.com>
2011-11-30 12:20:58 +0100

    Initial attempt at tab-completion. Get java.lang.ExceptionInInitializerError

--- lifty-plugin/Lifty.scala
+++ lifty-plugin/Lifty.scala
@@ -3,8 +3,13 @@ package org.lifty
 import java.net.{ URL }
 import java.io.{ File }
 import org.lifty.engine._
+
 import sbt._
 import Keys._
+import Defaults._
+import Project.Initialize
+import complete.DefaultParsers._
+import complete.{ Parser }
 import sbt.compiler.{ RawCompiler }
 import xsbti.{Logger}
 
@@ -13,93 +18,99 @@ import org.fusesource.scalate.support.{ Compiler }
 
 object Lifty extends Plugin {
 
-  override lazy val settings = Seq(commands += liftyCommand)
+  import LiftyParsers._
+  
+  // Keys
+  val XLifty    = config("xlifty")
+  val create    = InputKey[Unit]("create")
+  val templates = InputKey[Unit]("templates")
+  val learn     = InputKey[Unit]("learn")
+  val delete    = InputKey[Unit]("delete")
+  val upgrade   = InputKey[Unit]("upgrade")
+  val recipes   = TaskKey[Unit]("recipes")
+  val help      = TaskKey[Unit]("help")
 
-  lazy val liftyCommand = Command.args("lifty","<help>") { (state, args) =>
-    
-    val bytecodeDir = 
-      new java.io.File(List(System.getProperty("user.home"),".lifty_workspace","classes").mkString(File.separator))
-    
-    bytecodeDir.mkdirs() 
-    
-    // classpath
-    
-    val cp = {
-      val scalaCompilerPath = state.configuration.provider.scalaProvider.compilerJar
-      val scalaLibraryPath = state.configuration.provider.scalaProvider.libraryJar
-      val scalatePath = new java.io.File(List(
-        System.getProperty("user.home"),
-        ".ivy2",
-        "cache",
-        "org.fusesource.scalate",
-        "scalate-core",
-        "bundles",
-        "scalate-core-1.5.3.jar"
-      ).mkString(File.separator))
-      val scalateUtilPath = new java.io.File(List(
-        System.getProperty("user.home"),
-        ".ivy2",
-        "cache",
-        "org.fusesource.scalate",
-        "scalate-util",
-        "bundles",
-        "scalate-util-1.5.3.jar"
-      ).mkString(File.separator))
-      (scalaLibraryPath :: scalaCompilerPath :: scalatePath :: scalateUtilPath :: Nil)
-    }
-    
-    //  SBTCompiler
-    class SBTCompiler extends Compiler {
-      
-      val cpopts = (new ClasspathOptions(bootLibrary = false, 
-                                         compiler = true, 
-                                         extra = true, 
-                                         autoBoot = false,
-                                         filterLibrary = false)): ClasspathOptions
-      
-      def compile(file: File): Unit = {
-        
-        val scalaProvider = state.configuration.provider.scalaProvider
-        val scalaInstance = new ScalaInstance(version = scalaProvider.version, 
-                                              explicitActual = None, /* not sure if this is correct.*/
-                                              libraryJar = scalaProvider.libraryJar, 
-                                              compilerJar = scalaProvider.compilerJar, 
-                                              loader = scalaProvider.loader,
-                                              extraJars = scalaProvider.jars)
-
-        val compiler = new RawCompiler(scalaInstance = scalaInstance, 
-                                       cp = cpopts, 
-                                       log = ConsoleLogger())
-        
-        compiler.apply(sources = List(file),
-                       classpath = cp,
-                       outputDirectory = bytecodeDir,
-                       options = Nil)
-      }
-    }
-    
-    class SBTTemplateEngine() extends TemplateEngine {
-      override protected def createCompiler: Compiler = {
-        new SBTCompiler()
-      }
-      override def bytecodeDirectory = bytecodeDir
-    } 
-     
-    // create the special engine. 
-    val engine = new SBTTemplateEngine()
-    engine.allowCaching = false 
-    
-    
-    // Run the lifty instance.    
-    new LiftyInstance(Some(engine)).run(args.toList).fold(
-      e => {
-        println("\n" + e.message + "\n")
-        state.fail
-      },
-      s => {
-        println("\n"+s+"\n")
-        state
-      }
-    )
+  val xliftySettings: Seq[sbt.Project.Setting[_]] = {
+    inConfig(XLifty)(Seq(
+      create    <<= InputTask(createParser)(createDef),
+      templates <<= InputTask(templatesParser)(templatesDef),
+      learn     <<= InputTask(learnParser)(learnDef),
+      delete    <<= InputTask(deleteParser)(deleteDef),
+      upgrade   <<= InputTask(upgradeParser)(upgradeDef),
+      recipes   := recipesTask,
+      help      := helpTask
+    ))
   }
+
+
+  // Task definitions
+
+  private val recipesTask = recipes := {
+    println("Would've listed recipes")
+  }
+
+  private val helpTask = help := {
+    println("Would've printed help msg")
+  }
+
+  private val createDef = (parsedTask: TaskKey[(String,String)]) => {
+    (parsedTask) map { case ( (recipe: String, template: String) ) =>
+      println("Would've created " + template + " of " + recipe)
+    }
+  }
+
+  private val templatesDef = (parsedTask: TaskKey[String]) => {
+    (parsedTask) map { case (recipe: String) =>
+      println("Templates of " + recipe)
+    }
+  }
+
+  private val learnDef = (parsedTask: TaskKey[(String,String)]) => {
+    (parsedTask) map { case (name: String, url: String) =>
+      println("Would've learned " + name + " as " + url)
+    }
+  }
+  private val deleteDef = (parsedTask: TaskKey[String]) => {
+    (parsedTask) map { case (recipe: String) =>
+      println("Would've deleted " + recipe)
+    }
+  }
+  private val upgradeDef = (parsedTask: TaskKey[String]) => {
+    (parsedTask) map { case (recipe: String) =>
+      println("Would've updated " + name)
+    }
+  }
+  
+  object LiftyParsers {
+
+    // List of available parsers:
+    // http://harrah.github.com/xsbt/latest/api/sbt/complete/DefaultParsers$.html
+
+    // Sample data for now.
+    val recipes = "lift" :: "sbt" :: Nil
+    val templates = Map(
+      "lift" -> List("project", "project-blank", "snippet", "layout"),
+      "sbt"  -> List("plugin")
+    )
+
+    val recipe: Parser[String] = Space ~> NotSpace.examples(recipes : _ *) <~ Space
+
+    val template: String => Parser[(String,String)] = (r) => NotSpace.examples(templates(r) : _ *) map ( t => (r,t) )
+
+    val name: Parser[String] = Space ~> NotSpace <~ Space
+
+    val url: String => Parser[(String,String)] = (n) => NotSpace map ( u => (n,u) )
+
+    // I want to get rid of this as I don't need access to the State or scalaVersion
+    def init[T](p: Parser[T]) = (scalaVersion) { s => (state: State) => p}
+
+    val createParser: Initialize[State => Parser[(String,String)]] = init(recipe flatMap template)
+    val templatesParser: Initialize[State => Parser[String]] = init(recipe)
+    val learnParser: Initialize[State => Parser[(String,String)]] = init(name flatMap url)
+    val deleteParser: Initialize[State => Parser[String]] = init(recipe)
+    val upgradeParser: Initialize[State => Parser[String]] = init(recipe)
+
+
+  }
+  
 }
--- lifty-plugin/build.sbt
+++ lifty-plugin/build.sbt
@@ -1,6 +1,6 @@
 sbtPlugin := true
 
-version := "1.7.3"
+version := "1.7.4-SNAPSHOT"
 
 name := "lifty"
 
